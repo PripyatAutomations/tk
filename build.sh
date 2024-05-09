378c31b31f85afecd56ff1052f154e47626626c7
Project Manager <84538339+PripyatAutomations@users.noreply.github.com>
2024-05-09 11:58:15 -0400

    added cron & patch to allow a local cron for managing our bits

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+[ -z "$UID" ] || SUDO=sudo
+
 cp ext/Chroma-Hash/jquery.chroma-hash.js www/js
-cp src/escapes.js/escapes.js www/js
-cp src/escapes.js/escapes.min.js www/js
-cp src/toastr/build/toastr.js.map www/js
-cp src/toastr/build/toastr.min.js www/js
-cp src/toastr/build/toastr.min.css www/css
-mkdir -p log/asterisk
-mkdir -p var/run/asterisk
-mkdir -p var/cron
+cp ext/toastr/build/toastr.js.map www/js
+cp ext/toastr/build/toastr.min.js www/js
+cp ext/toastr/build/toastr.min.css www/css
+$SUDO mkdir -p log/asterisk
+$SUDO mkdir -p var/run/asterisk
+$SUDO mkdir -p var/cron
+
+# build modified cron (path fixes), if needed
+if [ ! -f bin/cron ]; then
+   if [ ! -f ext/cron/.tk-patched ]; then
+      (cd ext; patch -p0 < ../patches/0100-cron-paths.diff)
+   fi
+   make -C ext/cron
+   cp ext/cron/cron sbin
+   cp ext/cron/crontab bin
+fi
